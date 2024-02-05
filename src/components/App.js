14b9dd058555a98af22f52eea86686c5319f985c
Imran shaikh <imranpathan7040@gmail.com>
2024-02-05 15:07:50 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,43 @@
-import React,{useState} from "react";
+import React,{useState,useMemo} from "react";
 
-
+const expensiveCalculation = (num) => {
+    console.log("Calculating...");
+    for (let i = 0; i < 1000000000; i++) {
+      num += 1;
+    }
+    return num;
+  };
 function App(){
-    const [todos, setTodos] = useState(['New todo']);
     const [count, setCount] = useState(0);
-    const [memoInput, setMemoInput] = useState('');
+    const [todos, setTodos] = useState([]);
+    const calculation = useMemo(() => expensiveCalculation(count), [count]);
+
+    const increment = () => {
+        setCount((c) => c + 1);
+      };
+      const addTodo = () => {
+        setTodos((t) => [...t, "New Todo"]);
+      };
   
-    const addTodo = () => {
-      setTodos([...todos, 'New todo']);
-    };
-  
-    const incrementCount = () => {
-      setCount(count + 1);
-    };
-  
-    const addItem = () => {
-      if (memoInput.length > 5) {
-        setTodos([...todos, memoInput]);
-        setMemoInput('');
-      } else {
-        alert('Please enter a text with more than 5 characters.');
-      }
-    };
 
     return (
 
-        <div className="app" id="main">
-             <button onClick={addTodo}>Add Todo</button>
-
-{/* Increment Button */}
-<button onClick={incrementCount} id="add-todo-btn">Increment Count</button>
-<span id="incr-cnt">{count}</span>
-
-{/* Memo Input and Submit Button */}
-<div>
-  <input
-    type="text"
-    id="skill-input"
-    value={memoInput}
-    onChange={(e) => setMemoInput(e.target.value)}
-    placeholder="Type here..."
-  />
-  <button onClick={addItem}>Submit</button>
-</div>
-
-{/* Todo List */}
-<ul>
-  {todos.map((todo, index) => (
-    <li key={index}>{todo}</li>
-  ))}
-</ul>
+        <div>
+        <div>
+          <h2>My Todos</h2>
+          {todos.map((todo, index) => {
+            return <p key={index}>{todo}</p>;
+          })}
+          <button onClick={addTodo}>Add Todo</button>
         </div>
+        <hr />
+        <div>
+          Count: {count}
+          <button onClick={increment}>+</button>
+          <h2>Expensive Calculation</h2>
+          {calculation}
+        </div>
+      </div>
     )
 }
 
